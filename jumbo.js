6fb7f4dc2d494db098a179bb4f5eaf3abb67a02f
nikonojo <163209927+nikonojo@users.noreply.github.com>
2024-03-22 17:09:27 -0300

    hehe

--- jumbo.js
+++ jumbo.js
@@ -1,6 +1,25 @@
-function changeImage()
+var currentImage = 0;
+let currentPage = 0;
+let imageArray = [
+    "Images/jumbo-1.png",
+    "Images/jumbo-2.jpg",
+    "Images/jumbo-3.jpg"
+];
+
+function changeJumboImage()
 {
-var img = document.getElementById("image");
-img.src="Images/jumbo-2.jpg";
+var img = document.getElementById("jumbo-image");
+img.src=imageArray[currentImage];
+if(currentImage == 1){ currentImage = 0; return; }
+else if(currentImage == 0){ currentImage = 1; return; }
 return false;
+}
+
+function SwitchPage(){
+    currentPage++;
+    if(currentPage > 2){ currentPage = 0; }
+
+    var img = document.getElementById("jumbo-image");
+    img.src=imageArray[currentPage];
+    return false;
 }
