d307f5952bbf564b82171920b03b7b989b361e21
nikonojo <163209927+nikonojo@users.noreply.github.com>
2024-03-27 15:40:38 -0300

    wow

--- jumbo.js
+++ jumbo.js
@@ -2,8 +2,13 @@ var currentImage = 0;
 let currentPage = 0;
 let imageArray = [
     "Images/jumbo-1.png",
-    "Images/jumbo-2.jpg",
+    "Images/jumbo-2.png",
     "Images/jumbo-3.jpg"
+];
+let textArray = [
+    'PROJECT: STARLIGHT',
+    'DANN FOX & THE TIME MACHINE',
+    'cofe hehe'
 ];
 
 function changeJumboImage()
@@ -15,11 +20,28 @@ else if(currentImage == 0){ currentImage = 1; return; }
 return false;
 }
 
-function SwitchPage(){
-    currentPage++;
-    if(currentPage > 2){ currentPage = 0; }
+function SwitchPage(index){
+    if(index == 1){
+        currentPage++;
+        if(currentPage > 2){ currentPage = 0; }
+    
+        var img = document.getElementById("jumbo-image");
+        img.src=imageArray[currentPage];
 
-    var img = document.getElementById("jumbo-image");
-    img.src=imageArray[currentPage];
-    return false;
+        var txt = document.getElementById("jumbo-text");
+        txt.textContent = textArray[currentPage];
+        return false;
+    }
+
+    else{
+        currentPage--;
+        if(currentPage < 0){ currentPage = 2; }
+    
+        var img = document.getElementById("jumbo-image");
+        img.src=imageArray[currentPage];
+
+        var txt = document.getElementById("jumbo-text");
+        txt.textContent = textArray[currentPage];
+        return false;
+    }
 }
